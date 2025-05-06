e46bb7a4964cd43b67e3649f6ec307fe4694c0d9
insystemgrp <insystemgrp@gmail.com>
2025-05-06 15:49:27 +0530

    check standard

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,9 +7,5 @@ abstract class Controller
     public function demo(){return TRUE;
     }
 
-    public function demo2()
-    {
-        return False;
-    }
-
+    public function demo2(){return False;}
 }
